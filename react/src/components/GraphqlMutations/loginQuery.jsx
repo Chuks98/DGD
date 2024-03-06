5e82b9d0553cdd2b179a7b41c15abc334ce40db1
Chuks <chukwumaonyedika98@gmail.com>
2024-03-06 15:20:06 +0100

    6th commit

--- react/src/components/GraphqlMutations/loginQuery.jsx
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const ADMIN_LOGIN_QUERY = gql`
-  query Login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
-      username
-    }
-  }
-`;
--- react/src/components/GraphqlMutations/subscribeMutation.jsx
+++
@@ -1,9 +0,0 @@
-import {gql} from '@apollo/client';
-
-export const Subscribe = gql`
-    mutation Subscribe($emailSubscription: EmailSubscription!) {
-        subscribe(emailSubscription: $emailSubscription) {
-            email
-        }
-    }
-`;
