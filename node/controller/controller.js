7c5707d32fd5b30020b6034a51fc499a0a3b46e2
Chukwuma <chukwumaonyedika98@gmail.com>
2024-04-11 10:59:49 +0100

    38th commit

--- node/controller/controller.js
+++ node/controller/controller.js
@@ -34,4 +34,4 @@ const sendThumbnail = (req, res) => {
     });
   };
 
-  module.exports = { sendAudio, sendThumbnail };
+module.exports = { sendAudio, sendThumbnail };
--- node/router/router.js
+++ node/router/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controller/controller').default;
+const controller = require('../controller/controller');
 
 router.post('/sendAudio', controller.sendAudio);
 router.post('/sendThumbnail', controller.sendThumbnail);
