12795c191234ca5f7463be4cec278913ae2eb678
Chukwuma <chukwumaonyedika98@gmail.com>
2024-04-11 13:33:30 +0100

    38th commit

--- node/controller/controller.js
+++ node/controller/controller.js
@@ -4,7 +4,7 @@ const sendAudio = (req, res) => {
   // Assuming you have a field named 'audio' in your form
   devotionAudio.single('audio')(req, res, (err) => {
     if (err) {
-      console.log(req)
+      console.log("This is the request", req)
       // Handle Multer error (e.g., file type not allowed)
       console.error('Error uploading audio:', err);
       return res.status(400).send('Error uploading audio');
